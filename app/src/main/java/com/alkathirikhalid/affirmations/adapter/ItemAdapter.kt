c2d73af0a401c493bed5e8651235714d12271ba8
alkathirikhalid <khalid@alkathirikhalid.com>
2023-11-02 10:05:35 +0800

    - Create a list with RecyclerView - RecyclerView class - RecyclerView Adapters - RecyclerView ViewHolder - Lists in Material Design - Cards in Material Design - MaterialCardView - Getting Started with Material Components for Android - Android Styling: Themes vs. Styles - Adaptive icons

--- app/src/main/java/com/alkathirikhalid/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/alkathirikhalid/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.alkathirikhalid.affirmations.R
@@ -23,6 +24,7 @@ class ItemAdapter(
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -42,6 +44,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
--- app/src/main/java/com/alkathirikhalid/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/alkathirikhalid/affirmations/model/Affirmation.kt
@@ -1,3 +1,9 @@
 package com.alkathirikhalid.affirmations.model
 
-data class Affirmation(val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
