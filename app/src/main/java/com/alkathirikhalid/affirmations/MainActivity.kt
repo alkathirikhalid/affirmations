73d9cb221abd3b9a05ce4b3baed25aca80d8f668
alkathirikhalid <khalid@alkathirikhalid.com>
2023-11-02 10:23:06 +0800

    Beautify App

--- app/src/main/java/com/alkathirikhalid/affirmations/MainActivity.kt
+++ app/src/main/java/com/alkathirikhalid/affirmations/MainActivity.kt
@@ -3,6 +3,7 @@ package com.alkathirikhalid.affirmations
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.alkathirikhalid.affirmations.adapter.ItemAdapter
 import com.alkathirikhalid.affirmations.data.Datasource
 
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         val myDataset = Datasource().loadAffirmations()
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        // Set GridLayoutManager with 2 columns
+        //recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.layoutManager = // Staggered layout
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
         // Use this setting to improve performance if you know that changes
